3c73829e0ba8e620a2470d9bd55151b2222e08c4
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-10-19 22:20:49 +0100

    add project to the domain to make it unique

--- terraform/pool.tf
+++ terraform/pool.tf
@@ -59,7 +59,7 @@ output "aws_cognito_user_pool_id" {
 }
 
 resource "aws_cognito_user_pool_domain" "user_pool_domain_name" {
-  domain          = local.dev_auth_domain_name
+  domain          = "${local.project}-${local.dev_auth_domain_name}"
   user_pool_id    = aws_cognito_user_pool.pool.id
 
   # FIXME: for prod it should be custom domain
