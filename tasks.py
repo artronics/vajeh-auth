b86932ec493664c0e2da5f831389eaa1b99fbad9
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-02-21 21:21:07 +0000

    refactor tasks.py and use json env

--- tasks.py
+++ tasks.py
@@ -1,14 +1,14 @@
+import json
 import os
 import subprocess
 from pathlib import Path
 
 from invoke import task
 
-ENV_FILE = ".env"
 PERSISTENT_WORKSPACES = ["dev", "prod"]
 ROOT_ZONE = "vajeh.co.uk"
 
-default_kvm = {
+default_conf = {
     "PROJECT": os.getenv("PROJECT", Path(os.getcwd()).stem),
     "ENVIRONMENT": os.getenv("ENVIRONMENT", "dev"),
     "WORKSPACE": os.getenv("WORKSPACE", "dev"),
@@ -18,52 +18,34 @@ default_kvm = {
 }
 
 
-def update_kvm():
+def load_project_conf():
+    with open(f"{os.getcwd()}/project.env.json", 'r') as public_conf_file:
+        public_conf = json.load(public_conf_file)
+
     try:
-        with open(ENV_FILE) as f:
-            kv = {k.strip(): v.strip() for k, v in (line.split('=') for line in f if line.strip())}
-    except ValueError:
-        print("Parse error. Format file like key=value")
-        exit(1)
+        with open(f"{os.getcwd()}/project.private.env.json", 'r') as private_conf_file:
+            private_conf = json.load(private_conf_file)
     except FileNotFoundError:
-        print("Environment file not found. Using only default values and environment variables.")
-        return default_kvm
-    return default_kvm | kv
+        print(
+            "Private environment file not found. "
+            "Using only default values and environment variables for private settings.")
+        private_conf = default_conf
+
+    return default_conf | public_conf | private_conf
 
 
-kvm = update_kvm()
-# .env kvm will overwrite environment variables
-os.environ.update(kvm)
-# DO NOT print the whole kvm. There are secrets in there
+config = load_project_conf()
+# config will overwrite environment variables
+os.environ.update(config)
+# DO NOT print the whole config. There are secrets in there
 print("Settings:")
 print(
-    f"PROJECT: {kvm['PROJECT']}\nENVIRONMENT: {kvm['ENVIRONMENT']}\nWORKSPACE: {kvm['WORKSPACE']}\nTERRAFORM_DIR: {kvm['TERRAFORM_DIR']}\n")
+    f"PROJECT: {config['PROJECT']}\nENVIRONMENT: {config['ENVIRONMENT']}\n"
+    f"WORKSPACE: {config['WORKSPACE']}\nTERRAFORM_DIR: {config['TERRAFORM_DIR']}\n")
 
-ACCOUNT = "ptl" if kvm['ENVIRONMENT'] != "prod" else "prod"
-
-
-def get_state_s3_name():
-    prj = kvm['PROJECT']
-    return f"{prj}-{ACCOUNT}-terraform-state"
-
-
-TERRAFORM_STATE_S3 = get_state_s3_name()
-
-
-def get_tf_vars(ws):
-    workspace_tag = ws
-    if ws not in PERSISTENT_WORKSPACES and not ws.startswith("pr-"):
-        workspace_tag = f"user-{ws}"
-
-    account_zone = f"{ACCOUNT}.{ROOT_ZONE}"
-
-    all_vars = {"project": kvm["PROJECT"], "workspace_tag": workspace_tag, "account_zone": account_zone}
-
-    tf_vars = ""
-    for k, v in all_vars.items():
-        tf_vars += f"-var=\"{k}={v}\" "
-
-    return tf_vars
+aws_account = "ptl" if config['ENVIRONMENT'] != "prod" else "prod"
+tf_state_bucket = f"{config['PROJECT']}-{aws_account}-terraform-state"
+tf_dir = config["TERRAFORM_DIR"]
 
 
 def parse_workspace_list(output):
@@ -79,78 +61,91 @@ def parse_workspace_list(output):
     return workspaces, current_ws
 
 
-def get_terraform_workspaces(_dir) -> (list[str], str):
-    s = subprocess.check_output(["terraform", f"-chdir={_dir}", "workspace", "list"])
+def get_terraform_workspaces() -> (list[str], str):
+    s = subprocess.check_output(["terraform", f"-chdir={tf_dir}", "workspace", "list"])
     return parse_workspace_list(s.decode("utf-8"))
 
 
-def switch_workspace(_dir, ws):
-    subprocess.run(["terraform", f"-chdir={_dir}", "workspace", "select", ws])
+def switch_workspace(ws):
+    subprocess.run(["terraform", f"-chdir={tf_dir}", "workspace", "select", ws])
 
 
-def create_workspace(_dir, ws):
-    subprocess.run(["terraform", f"-chdir={_dir}", "workspace", "new", ws])
+def create_workspace(ws):
+    subprocess.run(["terraform", f"-chdir={tf_dir}", "workspace", "new", ws])
 
 
-def delete_workspace(_dir, ws):
-    (_, current) = get_terraform_workspaces(_dir)
+def delete_workspace(ws):
+    (_, current) = get_terraform_workspaces()
     if ws == "default" or current == "default":
         return
-    switch_workspace(_dir, "default")
-    subprocess.run(["terraform", f"-chdir={_dir}", "workspace", "delete", ws])
+    switch_workspace("default")
+    subprocess.run(["terraform", f"-chdir={tf_dir}", "workspace", "delete", ws])
 
 
-@task(help={"dir": "Directory where terraform files are located. Set default via TERRAFORM_DIR in env var or .env file",
-            "ws": "Terraform workspace. Set default via WORKSPACE in env var or .env file"})
-def workspace(c, dir=kvm["TERRAFORM_DIR"], ws=kvm["WORKSPACE"]):
-    (wss, current_ws) = get_terraform_workspaces(dir)
+def get_tf_vars():
+    (_, ws) = get_terraform_workspaces()
+    workspace_tag = ws
+    if ws not in PERSISTENT_WORKSPACES and not ws.startswith("pr-"):
+        workspace_tag = f"user-{ws}"
+
+    account_zone = f"{aws_account}.{ROOT_ZONE}"
+
+    all_vars = {"project": config["PROJECT"], "workspace_tag": workspace_tag, "account_zone": account_zone}
+
+    tf_vars = ""
+    for k, v in all_vars.items():
+        tf_vars += f"-var=\"{k}={v}\" "
+
+    return tf_vars
+
+
+@task(help={"ws": "Terraform workspace. Set default via WORKSPACE in env var or .env file"})
+def workspace(c, ws=config["WORKSPACE"]):
+    (wss, current_ws) = get_terraform_workspaces()
     if ws not in wss:
-        create_workspace(dir, ws)
+        create_workspace(ws)
     elif ws != current_ws:
-        switch_workspace(dir, ws)
+        switch_workspace(ws)
 
 
-@task(help={"dir": "Directory where terraform files are located. "
-                   "Set default via TERRAFORM_DIR in env var or .env file"})
-def init(c, dir=kvm["TERRAFORM_DIR"]):
-    c.run(f"terraform -chdir={dir} init -backend-config=\"bucket={TERRAFORM_STATE_S3}\"", in_stream=False)
+@task()
+def init(c):
+    c.run(f"terraform -chdir={tf_dir} init -backend-config=\"bucket={tf_state_bucket}\"", in_stream=False)
     print("DO NOT FORGET to run `provider-lock` task if, you added new provider/plugin.")
 
 
 @task(workspace)
-def plan(c, dir=kvm["TERRAFORM_DIR"]):
-    (_, ws) = get_terraform_workspaces(dir)
-    tf_vars = get_tf_vars(ws)
-    c.run(f"terraform -chdir={dir} plan {tf_vars}", in_stream=False)
+def plan(c):
+    tf_vars = get_tf_vars()
+    c.run(f"terraform -chdir={tf_dir} plan {tf_vars}", in_stream=False)
 
 
 @task(workspace)
-def apply(c, dir=kvm["TERRAFORM_DIR"]):
-    (_, ws) = get_terraform_workspaces(dir)
-    tf_vars = get_tf_vars(ws)
-    c.run(f"terraform -chdir={dir} apply {tf_vars} -auto-approve", in_stream=False)
+def apply(c):
+    tf_vars = get_tf_vars()
+    c.run(f"terraform -chdir={tf_dir} apply {tf_vars} -auto-approve", in_stream=False)
 
 
 @task(workspace)
-def destroy(c, dir=kvm["TERRAFORM_DIR"], dryrun=True):
-    (_, ws) = get_terraform_workspaces(dir)
-    tf_vars = get_tf_vars(ws)
+def destroy(c, dryrun=True):
+    (_, ws) = get_terraform_workspaces()
+    tf_vars = get_tf_vars()
     if dryrun:
-        c.run(f"terraform -chdir={dir} plan {tf_vars} -destroy", in_stream=False)
+        c.run(f"terraform -chdir={tf_dir} plan {tf_vars} -destroy", in_stream=False)
     else:
-        c.run(f"terraform -chdir={dir} destroy {tf_vars} -auto-approve", in_stream=False)
-        delete_workspace(dir, ws)
+        c.run(f"terraform -chdir={tf_dir} destroy {tf_vars} -auto-approve", in_stream=False)
+        delete_workspace(ws)
 
 
 @task(workspace)
-def output(c, dir=kvm["TERRAFORM_DIR"]):
+def output(c):
     c.run("mkdir -p build", in_stream=False)
-    c.run(f"terraform -chdir={dir} output -json", in_stream=False)
+    c.run(f"terraform -chdir={tf_dir} output -json", in_stream=False)
 
 
 @task(workspace)
-def lock_provider(c, dir=kvm["TERRAFORM_DIR"]):
+def lock_provider(c):
     print("This will take a while. Be patient!")
-    c.run(f"terraform -chdir={dir} providers lock "
+    c.run(f"terraform -chdir={tf_dir} providers lock "
           f"-platform=darwin_arm64 -platform=darwin_amd64 -platform=linux_amd64 -platform=windows_amd64",
           in_stream=False)
