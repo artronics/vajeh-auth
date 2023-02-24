9c34c3e17af7b0fe1db8ba749c7e6e73a61c4ff4
artronics <jalalhosseiny@gmail.com>
2023-02-24 21:27:32 +0000

    output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
+output "test_client_id" {
+  value = aws_cognito_user_pool_client.test_app.id
+}
+
+output "test_client_scopes" {
+  value = aws_cognito_user_pool_client.test_app.allowed_oauth_scopes
+}
+
 output "user_pool_id" {
   value = aws_cognito_user_pool.pool.id
 }
